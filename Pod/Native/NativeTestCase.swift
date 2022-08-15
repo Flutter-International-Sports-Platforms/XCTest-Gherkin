4c437469f48e6ece3237fcf52f58b33bfab99111
Tom Leighton <tom_leighton@starsgroup.com>
2022-08-15 13:01:54 +0100

    make path to params file generic

--- Pod/Native/NativeTestCase.swift
+++ Pod/Native/NativeTestCase.swift
@@ -56,7 +56,7 @@ open class NativeTestCase: XCGNativeInitializer {
             return []
         }
 
-        if let globalParamFile = Bundle.main.path(forResource: "PlugIns/GherkinUITests.xctest/globalParam", ofType: "json") {
+        if let globalParamFile = Bundle.main.path(forResource: "\(ProcessInfo.processInfo.environment["XCTestBundlePath"])/globalParam", ofType: "json") {
             do {
                 let jsonData = try NSData(contentsOfFile: globalParamFile, options: .mappedIfSafe)
                 ParseState.globalParams = try JSONSerialization.jsonObject(with: jsonData as Data, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary
